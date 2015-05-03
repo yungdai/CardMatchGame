11d76e4464543e1945debff2358381a4fef43a40
Yung Dai <yungchidai@gmail.com>
2015-05-03 14:58:06 -0400

    Initial Commit

---
+++ CardMatchGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardMatchGame
+//
+//  Created by Yung Dai on 2015-05-03.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CardMatchGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardMatchGame
+//
+//  Created by Yung Dai on 2015-05-03.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
